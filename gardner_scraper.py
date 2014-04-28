ad82a64bdc24bccbb04d455ef19ab9ea4ec1bf53
Hailan Pang <hpang@mit.edu>
2014-04-28 15:26:16 -0400

    Added scrapers for Gardner and Peabody Essex museums

---
+++ gardner_scraper.py
@@ -0,0 +1,77 @@
+from urllib2 import urlopen
+import re 
+
+from bs4 import BeautifulSoup
+
+def make_soup(url): 
+	html = urlopen(url).read()
+	return BeautifulSoup(html)
+
+#From base url, get all navigation links
+def get_nav_links(section_url):
+    soup = make_soup(section_url)
+    nav = soup.find('ul', {'id': 'nav'}) #find all links from navigation
+    #for every "li" found in nav, add to the link to a growing list 
+    navLinks = [BASE_URL + li.a["href"] for li in nav.findAll("li")]
+    return navLinks
+
+# From all navigation links, find current events and exhibitions
+def get_link_events(link_url): 
+	soup = make_soup(link_url)
+	events = []
+	content = soup.find('div', {'id':'content'}) # find content to search  
+	for s in content.findAll('span'): # find tags for current works 
+		if not (s and s.string): 
+			continue 
+		if s.string == 'Current Exhibitions': #find all current exhibitions, disregard past
+			currentEvents = soup.find('ul', {'class': 'subnav_ul divided'}) #get current events links
+			eventLinks = [BASE_URL+ li.a["href"] for li in currentEvents.findAll("li")] 
+			events = events + eventLinks 
+	return events  	
+
+# From current exhibition links, get relevant dates and information
+def get_event_info(event_url): 
+	soup = make_soup(event_url) 
+	content = soup.find('div', {'id':'content'}) # find content tag 
+	h1 = content.find('h1') # find title tag 
+	# em = h1.find('em')
+	exhName = h1.text # save exhibition name 
+
+	date = content.find('h4') #find date by this tag 
+	if not date: #otherwise, search for a different tag 
+		p = content.find('p', {'class': 'image_details'}) 
+		date =  p.find('strong')
+		if not date: # no date found
+			date = "" 
+	
+	exhDateLoc = date.text; # save dates and gallery location
+
+	return (exhName, exhDateLoc) 
+
+# From event pages, get all descriptions/text 
+def get_event_text(event_url): 
+	soup = make_soup(event_url)
+	div = soup.find('div', {'class': 'tab'}) # find div for paragraphs 
+	text = ""
+	for p in div.findAll('p'): 
+		text += p.getText() + '\n' # add paragraph texts to empty string 
+	return text 
+
+
+## Get information from Isabella Gardner Museum website ## 
+
+BASE_URL = "http://www.gardnermuseum.org"
+currentExhibitions = [] #list for event links
+eventInfo = {} #Dictionary stores ==> key (event/exhibition url): event/exhibition name, event date & loc, event descriptive text
+
+links = get_nav_links(BASE_URL) #get all navigation links from main page
+for link in links: 
+	if re.match('(.*)exhibition', link, re.I): #find all links with exhibitions 
+		currentExhibitions.append(get_link_events(link)) #all current event links 
+
+currentExhibitions = currentExhibitions[1:] # get rid of first in list, which is None
+
+for exhList in currentExhibitions: #iterate through to get to each exhibition link
+	for exh in exhList: 
+		eventInfo[exh] = get_event_info(exh), get_event_text(exh) # add information to dict with corresponding link
+
---
+++ peabody_scraper.py
@@ -0,0 +1,68 @@
+from urllib2 import urlopen
+import re 
+
+from bs4 import BeautifulSoup
+
+def make_soup(url): 
+	html = urlopen(url).read()
+	return BeautifulSoup(html)
+
+#From base url, get all navigation links
+def get_nav_links(section_url):
+    soup = make_soup(section_url)
+    nav = soup.find('ul', {'class': 'mainNav'}) #find all links from navigation
+    #for every "li" found in nav, add to the link to a growing list 
+    navLinks = [BASE_URL + li.a["href"] for li in nav.findAll("li")]
+    return navLinks
+
+# From all navigation links, find all links for events and exhibitions
+def get_link_events(link_url): 
+	soup = make_soup(link_url)
+	div = soup.find('div', {'class':'subNav'}) # find div to search  
+	eventLinks = [BASE_URL + li.a["href"] for li in div.findAll("li")] # find all urls for events and exhibitions
+	return eventLinks
+
+# From current exhibitions page, find links for current exhibitions
+def get_exhibitions(current_url): 
+	soup = make_soup(current_url)
+	content = soup.find('div', {'class': 'content'}) 
+	exhLinks = [BASE_URL + dt.a["href"] for dt in content.findAll("dt")] #build array of exhibition links
+	return exhLinks 
+
+# From current exhibition links, get relevant title, dates, and information 
+def get_event_info(event_url): 
+	soup = make_soup(event_url) 
+	feature = soup.find('div', {'class': 'feature_detail'}) #General wrapper for all event details
+	info = feature.find('div', {'class': 'info'}) 
+	exhTitle = info.find('h2').getText() # get exhibition title 
+
+	exhDatesLoc = "" #String to store dates and location 
+	for dates in feature.findAll('p', {'class':'dates'}): # iterate through tags to get dates and location
+		exhDatesLoc += dates.getText() 
+
+	text = "" # String to store all text for the exhibition 
+	for p in feature.findAll('p', {'style':'text-align: justify;'}): 
+		text += p.getText() 
+
+	return exhTitle, exhDatesLoc, text  
+
+## Get information from Peabody Essex Museum website ## 
+
+BASE_URL = "http://www.pem.org"
+
+exhibitions = [] #list for event links, including upcoming, current, touring, etc. 
+eventInfo = {} #Dictionary stores ==> key (event/exhibition url): event/exhibition name, event date & loc, event descriptive text
+
+links = get_nav_links(BASE_URL) #get all navigation links from main page
+for link in links: 
+	if re.match('(.*)exhibition', link, re.I): #find all links with exhibitions 
+		exhibitions = get_link_events(link) #all exhibition links 
+
+for exh in exhibitions: 
+	if re.match('(.*)current', exh, re.I): #find the link for current events (this can be changed for other desired links)
+		currentExhUrl = exh # find current exhibitions link 
+
+currentExhs = get_exhibitions(currentExhUrl) # array of all current exhibition links 
+for link in currentExhs: 
+	eventInfo[link] = get_event_info(link) # add all exhibition info to dictionary
+
