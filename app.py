651b843997e3818af8d9dba21b2cf025d12314b7
Liam Andrew <landrew@mit.edu>
2014-05-01 13:58:44 -0400

    Default to debug mode and new port

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, jsonify
 import gardner_scraper
 import peabody_scraper
+import harvard_art_scraper
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/gardner", method=['GET'])
-    return gardner_scraper.scrape()
+@app.route("/gardner")
+def scrape_gardner():
+    return jsonify(gardner_scraper.scrape())
 
-@app.route("/peabody", method=['GET'])
-	return peobody_scraper.scrape() 
+@app.route("/harvard")
+def scrape_harvard():
+	return jsonify(harvard_art_scraper.scrape())
 
-@app.route("/harvard", method=['GET'])
-	return harvard_art_scraper.scrape() 
+@app.route("/peabody")
+def scrape_peabody():
+	return jsonify(peabody_scraper.scrape()) 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=7000)
